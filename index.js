21f57695089571f22b448118d5603ff4eea2fbfa
SeverinuM <62368769+severinum@users.noreply.github.com>
2023-11-19 18:39:39 +0000

    Add node and mongo in Codespaces

---
+++ index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+
+(async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/gitsetup")
+        console.log("DB CONNECTED!");
+
+        const onListening = () => {
+            console.log("Listening on PORT 5000");
+        }
+
+        app.listen(5000, onListening)
+
+    } catch (error) {
+        console.error("error: ", error);
+        throw error;
+    }
+})()
